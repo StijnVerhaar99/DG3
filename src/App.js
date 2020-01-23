0e95d0b11663627a4ea0d243ed1e1f3715257331
StijnVerhaar99 <120487@student.drenthecollege.nl>
2020-01-23 13:25:16 +0100

    add changes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ function App() {
     <div className="App">
         <h1>test</h1>
         <p>Dit is de branch van stijn</p>
-<<<<<<< HEAD
         <p>Tekst van branch justin</p>
-=======
         <p>dit de de branch van stijn vanaf andere user</p>
->>>>>>> stijn
     </div>
   );
 }
