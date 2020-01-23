7b62c99d89ec2ec84d9fd7b3aca44f650254e15e
Stijn <120487@student.drenthecollege.nl>
2020-01-23 12:18:49 +0100

    derde commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> tweede commit
+          Edit <code>src/App.js</code> derde commit
         </p>
         <a
           className="App-link"
