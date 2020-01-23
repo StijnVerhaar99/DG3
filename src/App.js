7df30bd3be444ab914d2a94fd8fa7c149c799ed1
Stijn <120487@student.drenthecollege.nl>
2020-01-23 13:15:01 +0100

    eerste commit van andere user

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
         <h1>test</h1>
         <p>Dit is de branch van stijn</p>
+        <p>dit de de branch van stijn vanaf andere user</p>
     </div>
   );
 }
